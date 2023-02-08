30387be2b50082bb3f21584089f1593bc126e3d5
Kevin R. Thornton <krthornt@uci.edu>
2023-02-08 15:38:59 -0800

    bring back rune modules

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,7 @@ fn work() {
     // Code in string literal
     let rune_code = r###"
         pub fn callback_body(item) {
+            println!("rune got a value of {}", item.get_value);
             item.get_value * item.get_value
         }
     "###;
@@ -50,7 +51,11 @@ fn work() {
     let source = rune::Source::new("foo", rune_code);
     sources.insert(source);
 
-    let mut context = rune::Context::new();
+    // We need rune_modules default context so that
+    // stuff like io is allowed.
+    // A vanilla rune::Context has NO functionality
+    // from the rust std lib.
+    let mut context = rune_modules::default_context().unwrap(); //rune::Context::new();
     let mut m = rune::Module::new();
     m.field_fn(
         rune::runtime::Protocol::GET,
