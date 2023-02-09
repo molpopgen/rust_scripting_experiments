c99efbc93f5177c828fb1c73c2830c1bd7be91ff
Kevin R. Thornton <krthornt@uci.edu>
2023-02-09 10:23:28 -0800

    rune: inst_fn rather than field_fn for consistency w/lua

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ fn work() {
     // Code in string literal
     let rune_code = r###"
         pub fn callback_body(item) {
-            println!("rune got a value of {}", item.get_value);
-            item.get_value * item.get_value
+            println!("rune got a value of {}", item.get_value());
+            item.get_value() * item.get_value()
         }
     "###;
 
@@ -43,13 +43,8 @@ fn work() {
     // from the rust std lib.
     let mut context = rune_modules::default_context().unwrap(); //rune::Context::new();
     let mut m = rune::Module::new();
-    m.field_fn(
-        rune::runtime::Protocol::GET,
-        "get_value",
-        RuneCoreAPIType::get_value,
-    )
-    .unwrap();
     m.ty::<RuneCoreAPIType>().unwrap();
+    m.inst_fn("get_value", RuneCoreAPIType::get_value).unwrap();
     context.install(&m).unwrap();
     let result = rune::prepare(&mut sources).with_context(&context).build();
 
